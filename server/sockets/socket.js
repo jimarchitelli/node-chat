549fca518ce311d896aa05e5d3eb1aa3d0288a1f
Juan Marchitelli <jimarchitelli@gmail.com>
2019-07-25 19:32:52 -0300

    Chat con estilos

--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -20,17 +20,20 @@ io.on('connection', (client) => {
         let personasPorSala = usuarios.getPersonasPorSala(usuario.sala);
 
         client.broadcast.to(usuario.sala).emit('listaPersonas', personasPorSala);
+        client.broadcast.to(usuario.sala).emit('crearMensaje', crearMensaje('Administrador',`${usuario.nombre} se unió al chat`));
 
         callback(personasPorSala);
     });
 
 
-    client.on('crearMensaje', (data) => {
+    client.on('crearMensaje', (data, callback) => {
 
         let persona = usuarios.getPersona(client.id);
         let mensaje = crearMensaje(persona.nombre, data.mensaje);
         
-        client.broadcast.to(persona.sala).emit('crearMensaje', mensaje);
+        client.broadcast.to(persona.sala).emit('crearMensaje', mensaje);        
+        
+        callback(mensaje);
     });
 
     client.on('mensajePrivado', (data) => {
